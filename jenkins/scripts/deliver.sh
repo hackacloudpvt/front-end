c77becbc692e7654de51fa35252af6fbcdff5764
Augusto Bondanca <augustobondancaa@gmail.com>
2021-08-23 15:52:31 -0300

    jenkisfiile

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -6,19 +6,19 @@ echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
-npm run build
+yarn run build
 set +x
 
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
-echo 'The "npm start" command has a trailing ampersand so that the command runs'
+echo 'The "yarn start" command has a trailing ampersand so that the command runs'
 echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
+echo 'can pause running builds of CI/CD applications indefinitely. "yarn start"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "npm start") and writes this value to'
+echo 'of the previously run process (i.e. "yarn start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm start &
+yarn start &
 sleep 1
 echo $! > .pidfile
 set +x
